c5e392d5c8f9b50a6052b71d3c1f2e8579534302
NebulaOnion <nebulaonion@noreply.anonymousland.org>
2023-01-28 15:38:17 +0000

    Deprecate Drone CI (#83)
    Our Drone instance doesn't work anymore and Gitea is getting actions support very very soon.
    
    Reviewed-on: https://git.anonymousland.org/anonymousland/anonymousland/pulls/83

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run --rm \
-  --name=site \
-  --network=site \
-  --volume="$PWD:/srv/jekyll:Z" \
-  --volume="$PWD/vendor/bundle:/usr/local/bundle:Z" \
-  -it jekyll/jekyll \
-   gem update
